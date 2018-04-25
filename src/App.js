2a581e1c3e79ca4dba1e08473827f7033f414ae4
Vincent La <vincent.la@nokia.com>
2018-04-25 14:13:29 -0700

    adding in sticky header with a body

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Body from './body/body.jsx'
 
 class App extends Component {
+
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div className="App" onScroll={this.handleScroll}>
+        <div className="App-header">
+          <div className="title">Vincent La</div>
+        </div>
+        <div className="site-content">
+          <Body />
+        </div>
+      </div>;
   }
 }
 
---
+++ src/body/body.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Body extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <div className="content">
+      <div>Site content</div>
+      </div>;
+  }
+}
+
+export default Body;
