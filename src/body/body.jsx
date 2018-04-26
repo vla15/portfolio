723071dee09e55e66f515c120025a7bc7817b07f
Vincent La <vincent.la@nokia.com>
2018-04-26 10:25:08 -0700

    adding animation on scroll

--- src/body/body.jsx
+++ src/body/body.jsx
@@ -13,7 +13,7 @@ class Body extends React.Component {
       { image: "ct.jpeg", href: 'https://github.com/vla15/MagnanimousMonkeys77', title: "Check Thumbs", description: `A survey application allowing users to poll connected clients in real-time with the use of web sockets.  The results are then aggregated to determine an average score of those users who voted.` },
       { image: "nqu.jpeg", href: 'https://github.com/vla15/LuminousLeeks77', title: "nQu", description: `An application that enables business owners to monitor customers who are currently waiting in the queue through a real time, intuitive and clean UI. In addition, it will allow business owners to track the location of customers who have added themselves to the queue list and automatically send an SMS alert after a predefined time. Customers can also utilize this application to enqueue themselves to available wait lists within their proximity. It also enables the customer to actively monitor their expected wait times and the number of parties ahead of them.` }
     ];
-    return projects.map((project, i) => <Project key={i} href={project.href} image={project.image} title={project.title} description={project.description}/>)
+    return projects.map((project, i) => <Project index={i} key={i} href={project.href} image={project.image} title={project.title} description={project.description}/>)
   }
 
   render() {
--- src/project/project.jsx
+++ src/project/project.jsx
@@ -4,16 +4,40 @@ import './project.css';
 class Project extends React.Component {
   constructor(props) {
     super(props);
+    this.onScroll = this.onScroll.bind(this);
+    this.state ={
+      animate: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.onScroll)
+  }
+
+  onScroll() {
+    let el = document.getElementsByClassName('project-title')[this.props.index];
+    if (el.getBoundingClientRect().bottom <= window.innerHeight) {
+      this.setState({animate: true});
+      window.removeEventListener('scroll', this.onScroll);
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll);
   }
 
   render() {
     const { title, image, description, href } = this.props;
+    const animationState = {
+      animationName: 'topSlideIn',
+      animationDuration: '1.5s'
+    }
     const imageStyle = {
       backgroundImage: `url(${image})`
     }
     return <div className="project-content">
         <a href={href} className="project-title project-item">{title}</a>
-        <div className="project-image-box project-item" style={imageStyle} />
+        <div className="project-image-box project-item" style={this.state.animate ? Object.assign(animationState, imageStyle) : imageStyle} />
         <div className="project-description project-item">
           {description}
         </div>
