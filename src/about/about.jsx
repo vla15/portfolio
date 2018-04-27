95715ae2c01dd8805a9ffd5937066518d7b55bcc
Vincent La <vincent.la@nokia.com>
2018-04-26 20:15:23 -0700

    styling for the about me section

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import './about.css';
 
 const About = props => (
-  <div>
-    <div>About Me</div>
-    <div>
+  <div className="about-section">
+    <h2 className="section-header">About Me</h2>
+    <div className="about-box">
+      <div className="about-content">
       I’m a driven software engineer who is passionate about transforming ideas
       into dynamic full stack applications that can solve real-world problems. I
       place a large emphasis on creating rich and robust interfaces that are
       seamless and intuitive to users. I thrive in team-oriented environments
-      that value effective communication and transparency. I'm a huge advocate
+      that value effective communication and transparency.
+
+      I'm a huge advocate
       of blockchain technology, especially Ethereum, and believe it will lead to
       a new wave of disruptive applications that are secure and immutable.
+      </div>
     </div>
   </div>
 );
