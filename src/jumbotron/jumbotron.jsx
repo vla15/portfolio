f02aac8aba12d819ea4fc6df53b2338fca717d00
Vincent La <vincent.la@nokia.com>
2018-04-26 20:27:50 -0700

    having header handle mobile devices

--- src/jumbotron/jumbotron.jsx
+++ src/jumbotron/jumbotron.jsx
@@ -5,13 +5,13 @@ const JumboTron = (props) => {
   let imageStyle = {
     backgroundImage: 'url(main.jpg)'
   }
-  return (
-    <div className="jumbo-content">
-      <div className="jumbo-image" style={ imageStyle }></div>
-      <div className="jumbo-name">Vincent La</div>
-      <div className="jumbo-role">Software Engineer in San Francisco, CA</div>
-    </div>
-  )
+  return <div className="jumbo-content">
+      <div className="jumbo-image" style={imageStyle} />
+      <div className="jumbo-name jumbo-text">Vincent La</div>
+      <div className="jumbo-role jumbo-text">
+        Software Engineer in San Francisco, CA
+      </div>
+    </div>;
 }
 
 export default JumboTron;
