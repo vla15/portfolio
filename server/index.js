2f50a6e30f12b744a8c8b4f59fb1d888850087c4
Vincent La <vincela15@gmail.com>
2018-04-28 18:50:13 -0700

    implementing express server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,6 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('hello world'));
+
+app.listen(3000, () => console.log('app listening in on port', 3000));
