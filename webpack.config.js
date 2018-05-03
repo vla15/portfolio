2dd6390c76746d586c986c59c57442972dd573d3
Vincent La <vincela15@gmail.com>
2018-05-02 17:25:33 -0700

    striping out boilerplate configs.  Rebuilding webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        publicPath: "/public",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js(x?)$/,
+                enforce: "pre",
+                exclude: /node_modules/,
+                loader: "eslint-loader"
+            },
+            {
+                test: /\.(js|jsx)?$/,
+                include: path.join(__dirname, "client/src"),
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [["env", { modules: false }], "react"]
+                    }
+                }
+            }
+        ]
+    }
+}
