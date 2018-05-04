7038c54295420cd1ce92cd3b06b39989cd801b9a
Vincent La <vincela15@gmail.com>
2018-05-04 13:42:40 -0700

    installing nodemon.  fixing distribution from express server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/', (req, res) => res.send('hello world'));
+app.get('/', (req, res) => { res.status(200).sendFile(path.join(__dirname, '../src/index.html'))});
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.listen(3000, () => console.log('app listening in on port', 3000));
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Body from './body/body.jsx';
 import Header from './header/header.jsx';
